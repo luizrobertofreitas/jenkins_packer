3dff8ca0b097d97d9ab63fbba93209f02bd2643d
accurate <accurate@bender.(none)>
2014-08-06 17:54:29 -0300

    Criação do retorno como um hash

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,7 +9,7 @@ class HomeController < ApplicationController
 
 	files.each do |f|
 		md5 = Digest::MD5.file(f).hexdigest
-		@result.push(Hash.new(:file => f, :md5 => md5))
+		@result.push({:file => f, :md5 => md5})
 	end
   end
 
